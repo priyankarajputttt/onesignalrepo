91124992adf73f6b564a5d60c8c3537ce00d6983
priyankarajputttt <99882109+priyankarajputttt@users.noreply.github.com>
2023-12-03 00:27:39 +0530

    pp

--- src/App.js
+++ src/App.js
@@ -1,24 +1,65 @@
 import logo from './logo.svg';
 import './App.css';
-
+// import OneSignal from 'react-onesignal';
+import { useEffect } from 'react';
 function App() {
+  
+  useEffect(() => {
+    window.OneSignal = window.OneSignal || [];
+  const OneSignal = window.OneSignal;
+    OneSignal.push(()=> {
+      OneSignal.setExternalUserId("89898")
+      OneSignal.init(
+        {
+          appId: "be18a344-2f26-4152-a30c-dcb813f8905d", //STEP 9
+          promptOptions: {
+            slidedown: {
+              enabled: true,
+              actionMessage: "We'd like to show you notifications for the latest news and updates about the following categories.",
+              acceptButtonText: "OMG YEEEEESS!",
+              cancelButtonText: "NAHHH",
+              categories: {
+                  tags: [
+                      {
+                          tag: "react",
+                          label: "ReactJS",
+                      },
+                      {
+                        tag: "angular",
+                        label: "Angular",
+                      },
+                      {
+                        tag: "vue",
+                        label: "VueJS",
+                      },
+                      {
+                        tag: "js",
+                        label: "JavaScript",
+                      }
+                  ]
+              }     
+          } 
+        },
+        welcomeNotification: {
+          "title": "One Signal",
+          "message": "Thanks for subscribing!",
+        } 
+      },
+        //Automatically subscribe to the new_app_version tag
+        OneSignal.sendTag("new_app_version", "new_app_version", tagsSent => {
+          // Callback called when tag has finished sending
+          console.log('new_app_version TAG SENT', tagsSent);
+        })
+      );
+    });
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    // <div className="App">
+    //   <header className="App-header">
+        
+    //   </header>
+    // </div>
+    <h1>One Signal - React DEMO</h1>
   );
 }
 
